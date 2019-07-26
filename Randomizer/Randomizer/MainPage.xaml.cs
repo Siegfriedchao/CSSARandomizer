066e0b99e966d40ed2e61091a602f233325d808f
Youchao Wang <wang_youchao1234@126.com>
2019-07-26 22:58:39 +0100

    functionality complete

--- Randomizer/Randomizer/MainPage.xaml.cs
+++ Randomizer/Randomizer/MainPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -33,6 +35,9 @@ namespace Randomizer
         List<Rectangle> listRec = new List<Rectangle>();
 
         Boolean global_startFlag = false;
+        Boolean global_sweepFlag = false;
+
+        int global_RandomNumber = 0;
 
         public MainPage()
         {
@@ -141,6 +146,8 @@ namespace Randomizer
 
         private void Button_Click_1(object sender, RoutedEventArgs e) // reset
         {
+            global_sweepFlag = false;
+
             SolidColorBrush purpleBrush = new SolidColorBrush(Windows.UI.Colors.Purple);
             string chairLeft = "ms-appx:///Assets/chairLeft.png";
             Uri uriLeft = new Uri(chairLeft, UriKind.RelativeOrAbsolute);
@@ -199,7 +206,9 @@ namespace Randomizer
 
         private void Button_Click_2(object sender, RoutedEventArgs e) // paint
         {
-            if(global_startFlag)
+            global_sweepFlag = false;
+
+            if (global_startFlag)
             {
                 SolidColorBrush yellowBrush = new SolidColorBrush(Windows.UI.Colors.Yellow);
                 //rec1.Fill = whiteBrush;
@@ -218,11 +227,112 @@ namespace Randomizer
 
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private async void Button_Click(object sender, RoutedEventArgs e)
         {
             global_startFlag = true;
+            global_sweepFlag = true;
 
 
+            SolidColorBrush grayBrush = new SolidColorBrush(Windows.UI.Colors.Gray);
+
+            string chairLeft = "ms-appx:///Assets/chairLeft.png";
+            Uri uriLeft = new Uri(chairLeft, UriKind.RelativeOrAbsolute);
+            ImageBrush chairLeftBrush = new ImageBrush { ImageSource = new BitmapImage(uriLeft) };
+
+            string chairRight = "ms-appx:///Assets/chairRight.png";
+            Uri uriRight = new Uri(chairRight, UriKind.RelativeOrAbsolute);
+            ImageBrush chairRightBrush = new ImageBrush { ImageSource = new BitmapImage(uriRight) };
+
+            ImageBrush chairBrush;
+
+            chairBrush = chairRightBrush;
+
+            Random random = new Random();
+
+            int randomNumber;
+
+            int columnCounter = 0;
+            int rowCounter = 0;
+            Boolean flag = false;
+
+            /*
+             * Plotting a list of rectangles representing tables
+             */
+            for (var i = 0; i < 120; i++)
+            {
+                // five big columns with 12x2 per each column
+                if (rowCounter == 11)
+                {
+                    columnCounter++;
+
+                    rowCounter = 0;
+                }
+                else
+                {
+                    if (!flag)
+                    {
+                        flag = true;
+                    }
+                    else
+                    {
+                        rowCounter++;
+                    }
+                }
+
+                if (columnCounter % 2 == 0)
+                {
+                    chairBrush = chairLeftBrush;
+                }
+                else
+                {
+                    chairBrush = chairRightBrush;
+                }
+
+                listRec[i].Fill = chairBrush;
+            }
+
+
+                //playing a random trick here
+            while (global_sweepFlag)
+            {
+                randomNumber = random.Next(0, 120);
+                listRec[randomNumber].Fill = grayBrush;
+
+                await Task.Run(()=> Random_Trick());
+
+                await Task.Delay(TimeSpan.FromMilliseconds(100));
+                listRec[randomNumber].Fill = chairRightBrush;
+            }
+
+        }
+
+        private async Task Random_Trick()
+        {
+            //SolidColorBrush grayBrush = new SolidColorBrush(Windows.UI.Colors.Gray);
+
+            //string chairLeft = "ms-appx:///Assets/chairLeft.png";
+            //Uri uriLeft = new Uri(chairLeft, UriKind.RelativeOrAbsolute);
+            //ImageBrush chairLeftBrush = new ImageBrush { ImageSource = new BitmapImage(uriLeft) };
+
+            //string chairRight = "ms-appx:///Assets/chairRight.png";
+            //Uri uriRight = new Uri(chairRight, UriKind.RelativeOrAbsolute);
+            //ImageBrush chairRightBrush = new ImageBrush { ImageSource = new BitmapImage(uriRight) };
+
+            //ImageBrush chairBrush;
+
+            //chairBrush = chairRightBrush;
+
+            Random random = new Random();
+
+            //while (global_sweepFlag)
+            //{
+                global_RandomNumber = random.Next(0, 120);
+
+                //Thread.Sleep(1000);
+                //await delay(TimeSpan.FromSeconds(1));
+
+                //listRec[randomNumber].Fill = chairRightBrush;
+            //}
         }
     }
 }
